36c960ea1c5745c7bacc1bba368f4827e72c9baf
Ngu Truong <zquangu112z@gmail.com>
2020-05-18 12:59:16 +0700

    allow all inbound for Postgres

--- main.tf
+++ main.tf
@@ -1,27 +1,42 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_security_group" "default" {
-  vpc_id = "${data.aws_vpc.default.id}"
-  name   = "default"
+resource "random_string" "northwind-db-password" {
+  length = 32
+  upper = true
+  number = true
+  special = false
 }
 
-resource "aws_db_instance" "northwind-db" {
-    identifier = "northwind-db"
-    name = "northwind"
-    instance_class = "db.t2.small"
-    allocated_storage = 5
-    engine = "postgres"
-    engine_version = "11.4"
-    skip_final_snapshot = true
-    publicly_accessible = true
-    vpc_security_group_ids = ["${data.aws_security_group.default.id}"]
-    username = "postgres"
-    password = "postgres"
+resource "aws_security_group" "northwind" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "northwind"
+  description = "Allow all inbound for Postgres"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "northwind-ngutruong" {
+  identifier             = "northwind-ngutruong"
+  name                   = "northwind"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "11.4"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = ["${aws_security_group.northwind.id}"]
+  username               = "ngutruong"
+  password               = "${random_string.northwind-db-password.result}"
 }
